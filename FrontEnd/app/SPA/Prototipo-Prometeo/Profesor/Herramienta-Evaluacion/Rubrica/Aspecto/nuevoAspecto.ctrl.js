e551a7cab690a2d26e744e341f9d6ac451580b1b
Alejo14 <a.tapiat@pucp.pe>
2019-05-26 16:18:17 -0500

    Un criterio se puede editar y eliminar

--- FrontEnd/app/SPA/Prototipo-Prometeo/Profesor/Herramienta-Evaluacion/Rubrica/Aspecto/nuevoAspecto.ctrl.js
+++ FrontEnd/app/SPA/Prototipo-Prometeo/Profesor/Herramienta-Evaluacion/Rubrica/Aspecto/nuevoAspecto.ctrl.js
@@ -8,6 +8,7 @@ function($scope, $state, $stateParams, $uibModal, NgTableParams){
   };
   ctrl.rubricaId = $stateParams.id;
   ctrl.criteriosLista = [];
+
   ctrl.agregarCriterio = function () {
     var modalInstance = $uibModal.open({
       animation: false,
@@ -35,15 +36,66 @@ function($scope, $state, $stateParams, $uibModal, NgTableParams){
       }
     });
   };
+
+  ctrl.editarCriterio = function(indiceCriterio){
+    var modalInstance = $uibModal.open({
+      animation: false,
+      templateUrl: 'SPA/Prototipo-Prometeo/Profesor/Herramienta-Evaluacion/Rubrica/Criterio/nuevoCriterioModal.html',
+      controller: 'editarCriterioModalCtrl as ctrl',
+      size: 'lg',
+      backdrop: true,
+      keyboard: true,
+      resolve: {
+        parametros:  function(){
+          return {
+            idRubrica: ctrl.rubricaId,
+            criterio: ctrl.criteriosLista[indiceCriterio]
+          };
+        }
+      }
+
+    });
+
+    modalInstance.result.then( function (parametroRetorno) {
+      if (parametroRetorno) {
+          ctrl.criteriosLista[indiceCriterio] = parametroRetorno;
+      }
+    });
+  }
+
+  ctrl.eliminarCriterio = function (indiceCriterio){
+    swal({
+      title: "¿Esta seguro de que desea eliminar este criterio?",
+      text: "No podrá recuperar el criterio en el futuro",
+      icon: "warning",
+      buttons: {
+        cancelar: {
+          className: "btn btn-lg btn-danger"
+        },
+        confirm: {
+          text: "Sí, eliminar",
+          className: "btn btn-lg color-fondo-azul-pucp color-blanco"
+        }
+      },
+      closeModal: false
+    }).then(function (eliminarCriterioConfirmado) {
+      if (eliminarCriterioConfirmado !== "cancelar") {
+        ctrl.criteriosLista.splice(indiceCriterio,1);
+        $scope.$apply();
+      }
+    });
+  }
+
   ctrl.inicializarTabla = function () {
     ctrl.criteriosTabla = new NgTableParams({}, { dataset: ctrl.criteriosLista });
   }
+
   ctrl.regresarAspectos = function () {
     $state.go('nueva-rubrica', {id: ctrl.rubricaId});
   }
 
   ctrl.init = function () {
-    ctrl.inicializarTabla()
+    ctrl.inicializarTabla();
   }
 
   ctrl.init();
---
+++ FrontEnd/app/SPA/Prototipo-Prometeo/Profesor/Herramienta-Evaluacion/Rubrica/Criterio/editarCriterioModal.ctrl.js
@@ -0,0 +1,52 @@
+angular.module('vHackersModule').controller('editarCriterioModalCtrl', ['$scope','$uibModalInstance', 'parametros',
+function($scope, $uibModalInstance, parametros){
+  var ctrl = this;
+
+  ctrl.criterio = parametros.criterio;
+
+  ctrl.rubricaId = parametros.id;
+
+  ctrl.cerrar = function(){
+      $uibModalInstance.close(0);
+  };
+  ctrl.nivelesRubrica = [
+      {
+        id: "",
+        descripcion: "Alta",
+        puntaje: 10
+      },
+      {
+        id: "",
+        descripcion: "Media",
+        puntaje: 5
+      },
+      {
+        id: "",
+        descripcion: "Baja",
+        puntaje: 0
+      }
+    ];
+  ctrl.guardarCriterio = function () {
+    swal({
+      title: "¿Esta seguro de que desea agregar este criterio?",
+      text: "",
+      icon: "warning",
+      buttons: {
+        cancelar: {
+          text: "Cancelar",
+          className: "btn btn-lg btn-danger"
+        },
+        confirm: {
+          text: "Sí, agregar",
+          className: "btn btn-lg color-fondo-azul-pucp color-blanco"
+        }
+      },
+      closeModal: false
+    }).then(function (criterioNuevoConfirmado) {
+      if (criterioNuevoConfirmado !== "cancelar") {
+        $uibModalInstance.close(ctrl.criterio);
+      }
+    });
+  };
+
+}])
