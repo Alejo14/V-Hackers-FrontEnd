afe03728e1fba9908fbdfd36ffa149a43554fc1f
Anderson93PUCP <anderson.castillo@pucp.pe>
2019-06-11 12:10:03 -0500

    Visibilidad de es la Escala y creacion de la vista Escala

---
+++ FrontEnd/app/SPA/Prototipo-Prometeo/Profesor/Herramienta-Evaluacion/Escala/Criterio/nuevoCriterioModal.ctrl.js
@@ -0,0 +1,76 @@
+angular.module('vHackersModule').controller('nuevoCriterioCtrl', ['$scope','$uibModalInstance', 'nuevaEscalaService', 'parametros',
+function($scope, $uibModalInstance, nuevaEscalaService, parametros){
+  var ctrl = this;
+
+  ctrl.criterio = {
+    descripcion: "",
+    indicaciones: "",
+    puntaje_maximo: 0,
+    nivelesCriterio: []
+  }
+
+  ctrl.rubricaId = parametros;
+
+  ctrl.cerrar = function(){
+      $uibModalInstance.close(0);
+  };
+
+  ctrl.listarNiveles = function (){
+      idRubrica = {
+        "herramientaID" : ctrl.rubricaId
+      };
+      nuevaEscalaService.listarNiveles(idRubrica).then(function(nivelesListaData) {
+        ctrl.nivelesLista = nivelesListaData;
+        for(let i = 0; i < ctrl.nivelesLista.length; i++){
+          ctrl.nivelesLista[i].puntaje = 0;
+        }
+      });
+    }
+
+  ctrl.guardarCriterio = function () {
+    swal({
+      title: "¿Esta seguro de que desea agregar este criterio?",
+      text: "",
+      icon: "warning",
+      buttons: {
+        cancelar: {
+          text: "Cancelar",
+          className: "btn btn-lg btn-danger"
+        },
+        confirm: {
+          text: "Sí, agregar",
+          className: "btn btn-lg color-fondo-azul-pucp color-blanco"
+        }
+      },
+      closeModal: false
+    }).then(function (criterioNuevoConfirmado) {
+      if (criterioNuevoConfirmado !== "cancelar") {
+        console.log(ctrl.criterio);
+        ctrl.criterio.puntaje_maximo = ctrl.criterio.nivelesCriterio[0].puntaje;
+        angular.forEach(ctrl.criterio.niveles, function(nivel,indice){
+          if(ctrl.criterio.puntaje_maximo < nivel.puntaje){
+            ctrl.criterio.puntaje_maximo = nivel.puntaje;
+          }
+        });
+        $uibModalInstance.close(ctrl.criterio);
+      }
+    });
+  };
+
+  ctrl.nivelesPorCriterio = function(){
+    angular.forEach(ctrl.nivelesLista, function(nivel,indice){
+      var nivelCriterio = {
+        descripcion: "",
+        puntaje: 0
+      }
+      ctrl.criterio.niveles.push(nivelCriterio);
+    });
+  }
+
+  ctrl.init = function(){
+    ctrl.listarNiveles();
+    ctrl.nivelesPorCriterio();
+  }
+  ctrl.init();
+
+}])
