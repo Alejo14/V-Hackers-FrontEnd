190fecdf686dcb855c4248d8656b1460197ef355
CarlosGCA <carlosg.carrillo@pucp.pe>
2019-06-29 17:02:16 -0500

    QA Botones en la vista Grupos del profesor

--- FrontEnd/app/SPA/Prototipo-Prometeo/Profesor/Alumnos-Curso/Grupos/vistaActualizarGrupo.ctrl.js
+++ FrontEnd/app/SPA/Prototipo-Prometeo/Profesor/Alumnos-Curso/Grupos/vistaActualizarGrupo.ctrl.js
@@ -36,7 +36,7 @@ function($scope, $state, $stateParams, $uibModal, vistaGruposService, NgTablePar
   ctrl.actualizarGrupo = function(){
     console.log("hola");
       if (ctrl.grupo.nombre==""){
-        swal("¡Opss!", "Ingrese un nombre para el grupo por favor" , "error");
+        swal("¡Opss! Ingresa un nombre para el grupo", "" , "error");
       }
       else{
         swal({
