16a7211191044e956dccea0fd585eb5d47ed589a
hpunk <gustavoalzamora1997@gmail.com>
2019-06-30 22:07:50 -0500

    Swal para cargas masivas

--- FrontEnd/app/directivas/archivos/subidaArchivos.directive.js
+++ FrontEnd/app/directivas/archivos/subidaArchivos.directive.js
@@ -32,7 +32,7 @@ angular.module('vHackersModule').directive('subirArchivo', ['httpPostFactory','v
 
                   if (scope.eventoPostSeleccion) {
                       scope.eventoPostSeleccion({ nombre: element[0].files[0].name, tamano: element[0].files[0].size ,fechaCreacion: Date.now()}, scope.parametros);
-                      // swal("¡Bien hecho!", "El archivo se guardo exitosamente" , "success");
+                       swal("¡Bien hecho!", "El archivo se envió exitosamente" , "success");
                   }
                });
 
