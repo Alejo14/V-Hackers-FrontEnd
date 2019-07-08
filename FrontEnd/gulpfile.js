d8c511493861b9bd0fd084b5cb7a5f6ca793be15
CarlosGCA <carlosg.carrillo@pucp.pe>
2019-07-08 03:25:25 -0500

    Finalmente se logro el despligue en diferentes instancias

--- FrontEnd/gulpfile.js
+++ FrontEnd/gulpfile.js
@@ -1,74 +1,23 @@
-const gulp = require('gulp');
-const concat = require('gulp-concat');
-const browserSync = require('browser-sync').create();
+'use strict';
 
-const scripts = require('./scripts');
-const styles = require('./styles');
+var gulp = require('gulp');
+var wrench = require('wrench');
 
-var devMode = false;
-
-
-// gulp.task('css', function(){
-//   gulp.src('./app/**/*.css')
-//     .pipe(gulp.dest('./dist/'))
-//     .pipe(browserSync.reload({
-//       stream: true
-//     }));
-// });
-
-// gulp.task('js', function(){
-//   gulp.src('./app/**/*.js')
-//     .pipe(gulp.dest('./dist/'))
-//     .pipe(browserSync.reload({
-//       stream: true
-//     }));
-// });
-//
-// gulp.task('html', function(){
-//   gulp.src('./app/**/*.html')
-//     .pipe(gulp.dest('./dist/'))
-//     .pipe(browserSync.reload({
-//       stream: true
-//     }));
-// });
-//
-// gulp.task('ctrl.js', function(){
-//   gulp.src('./app/**/*.ctrl.js')
-//     .pipe(gulp.dest('./dist/'))
-//     .pipe(browserSync.reload({
-//       stream: true
-//     }));
-// });
-//
-// gulp.task('service.js', function(){
-//   gulp.src('./app/**/*.service.js')
-//     .pipe(gulp.dest('./dist/'))
-//     .pipe(browserSync.reload({
-//       stream: true
-//     }));
-// });
-//
-// gulp.task('build', function(){
-//   gulp.start(['css','js','html','ctrl.js','service.js']);
-// });
-
-gulp.task('browser-sync', function(){
-  browserSync.init(null, {
-    port: 8070,
-    open: false,
-    server: {
-      baseDir: 'app'
-    }
-  });
+/**
+ *  This will load all js or coffee files in the gulp directory
+ *  in order to load all gulp tasks
+ */
+wrench.readdirSyncRecursive('./gulp').filter(function(file) {
+  return (/\.(js|coffee)$/i).test(file);
+}).map(function(file) {
+  require('./gulp/' + file);
 });
 
-gulp.task('start', function() {
-  var devMode = true;
-  gulp.start(['browser-sync']);
-  //gulp.start(['build', 'browser-sync']);
-  // gulp.watch(['./app/**/*.css'],['css']);
-  // gulp.watch(['./app/**/*.js'],['js']);
-  // gulp.watch(['./app/**/*.ctrl.js'],['ctrl.js']);
-  // gulp.watch(['./app/**/*.service.js'],['service.js']);
-  // gulp.watch(['./app/**/*.html'],['html']);
+
+/**
+ *  Default task clean temporaries directories and launch the
+ *  main optimization build task
+ */
+gulp.task('default', ['clean'], function () {
+  gulp.start('build');
 });
