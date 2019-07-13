b001acb739af7a153f8e32749f7178b1d329947c
cesarurb <a20194664@pucp.edu.pe>
2019-07-13 18:43:56 -0500

    eliminar localhost, pdt: ya no pusheen eso

--- FrontEnd/app/variablesAmbiente.js
+++ FrontEnd/app/variablesAmbiente.js
@@ -2,7 +2,7 @@
   window.__env = window.__env || {};
   //API url
   //window.__env.apiUrl = 'http://200.16.7.187';
-  window.__env.apiUrl = 'http://localhost';
+  window.__env.apiUrl = 'http://200.16.7.187';
   //Url base
   window.__env.baseUrl = '/';
   //Puertos de microservicios
